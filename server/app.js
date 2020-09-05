ad987269a174f0689e09fa9a51219d81febb78d9
Marcus Sera <m.yoshiosera@gmail.com>
2020-09-04 20:49:04 -0700

    successfully retrieved data from twitter using twitter dependency

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const Twitter = require('twitter');
+const { twitterApiKey, twitterApiKeySecret, twitterBearerToken, twitterAccessToken, twitterAccessTokenSecret } = require('./config')
 
-const{ twitterBearerToken } = require('./config');
+// const{ twitterBearerToken } = require('./config');
 
 const app = express();
+
+const client = new Twitter({
+  consumer_key: `${twitterApiKey}`,
+  consumer_secret: `${twitterApiKeySecret}`,
+  access_token_key: `${twitterAccessToken}`,
+  access_token_secret: `${twitterAccessTokenSecret}`,
+});
+
+const params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, (error, tweets, response)=>{
+  if(!error){
+    console.log(tweets);
+  }
+  if(error){
+    console.log(error);
+  }
+});
 
 app.use(cors({
   origin: "http://localhost:3000",
@@ -14,24 +34,25 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-const config = {
-  method: 'get',
-  // url: `https://api.twitter.com/labs/2/users/by/username/${e.user}?expansions=pinned_tweet_id`,
-  url: `https://api.twitter.com/labs/2/users/by/username/${e.user}`,
-  headers: {
-    Authorization: `Bearer ${twitterBearerToken}`,
-    Cookie:
-      'personalization_id="v1_dLCv7GSVPP3SbawrnPi0+w=="; guest_id=v1%3A159899956972344374',
-  },
-};
 
-axios(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// const config = {
+//   method: 'get',
+//   // url: `https://api.twitter.com/labs/2/users/by/username/${e.user}?expansions=pinned_tweet_id`,
+//   url: `https://api.twitter.com/labs/2/users/by/username/${e.user}`,
+//   headers: {
+//     Authorization: `Bearer ${twitterBearerToken}`,
+//     Cookie:
+//       'personalization_id="v1_dLCv7GSVPP3SbawrnPi0+w=="; guest_id=v1%3A159899956972344374',
+//   },
+// };
+
+// axios(config)
+//   .then((response) => {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 app.use((err, req, res, next)=>{
   res.status(422).send({error: err.message});
--- server/config.js
+++ server/config.js
@@ -7,4 +7,6 @@ module.exports={
   twitterApiKey: process.env.API_KEY,
   twitterApiKeySecret: process.env.API_KEY_SECRET,
   twitterBearerToken: process.env.BEARER_TOKEN,
+  twitterAccessToken: process.env.ACCESS_TOKEN,
+  twitterAccessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
 };
